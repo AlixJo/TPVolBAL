7b2d578f6f41f54f0f985eb62753590e23e606c7
AlixJo <alix-jourdan@hotmail.fr>
2018-08-21 17:04:07 +0200

    DAO + test Alix 2 OK

--- vol-jpa/src/test/java/testdao/TestDaoLogin.java
+++ vol-jpa/src/test/java/testdao/TestDaoLogin.java
@@ -10,14 +10,14 @@ public class TestDaoLogin {
 	public static void main(String[] args) {
 		IdaoLogin daoLogin = new DaoLogin();
 
-//		Login L1 = new Login("AL13", "motdepasse1", false);
-//		daoLogin.save(L1);
-//		
-//		Login L2 = new Login("AL14", "motdepasse2", true);
-//		daoLogin.save(L2);
-//		
-//		Login L3 = new Login("AL15", "motdepasse3", false);
-//		daoLogin.save(L3);
+		Login L1 = new Login("AL13", "motdepasse1", false);
+		daoLogin.save(L1);
+		
+		Login L2 = new Login("AL14", "motdepasse2", true);
+		daoLogin.save(L2);
+		
+		Login L3 = new Login("AL15", "motdepasse3", false);
+		daoLogin.save(L3);
 		
 		
 		Login L4 = daoLogin.findById(11L);
