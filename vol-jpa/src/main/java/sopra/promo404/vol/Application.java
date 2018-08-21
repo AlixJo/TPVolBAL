be87640dd937c7ba2168ab0a934d948784fde076
AlixJo <alix-jourdan@hotmail.fr>
2018-08-21 12:05:41 +0200

    Création classe application + changement version snapshot

--- vol-jpa/src/main/java/sopra/promo404/vol/Application.java
+++
@@ -1,5 +0,0 @@
-package sopra.promo404.vol;
-
-public class Application {
-
-}
---
+++ vol-jpa/src/main/java/tpVolBAL/singleton/Application.java
@@ -0,0 +1,26 @@
+package tpVolBAL.singleton;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Application {
+
+		private static Application instance = null;
+
+		private final EntityManagerFactory emf = Persistence.createEntityManagerFactory("volpersist");
+
+		private Application() {
+
+		}
+
+		public static Application getInstance() {
+			if (instance == null) {
+				instance = new Application();
+			}
+
+			return instance;
+		}
+
+		public EntityManagerFactory getEmf() {
+			return emf;
+		}
+	}
