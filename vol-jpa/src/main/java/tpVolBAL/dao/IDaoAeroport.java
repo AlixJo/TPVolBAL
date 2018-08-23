ec9ba904cffa93679d292f523bb3ab8c585b0073
Systank <lucas.crappeel@laposte.net>
2018-08-23 15:53:01 +0200

    Exemple de changement de Dao to Repository

--- vol-jpa/src/main/java/tpVolBAL/dao/IDaoAeroport.java
+++ vol-jpa/src/main/java/tpVolBAL/dao/IDaoAeroport.java
@@ -2,6 +2,6 @@ package tpVolBAL.dao;
 
 import sopra.promo404.vol.model.Aeroport;
 
-public interface IDaoAeroport extends IDao<Aeroport, Long> {
+public interface IDaoAeroport extends JpaRepository<Aeroport, Long> {
 
 }
